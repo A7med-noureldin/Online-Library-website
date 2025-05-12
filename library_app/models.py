907b5a91048f8a49ceb6342d79877957093d5fc9
Vibin_alon3 <usif.dev@gmail.com>
2025-05-12 08:53:53 +0300

    Update models.py
    new

--- library_app/models.py
+++ library_app/models.py
@@ -12,3 +12,18 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_year = models.IntegerField()
+    publisher = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    cover_image = models.ImageField(upload_to='book_covers/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
